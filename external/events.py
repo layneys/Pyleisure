abe095a87e295c059231917c1be8a2869911c7d3
Kuchev Anton <kuchevanton@bk.ru>
2025-01-08 15:50:16 +0300

    added styles & refactored html conception

--- external/events.py
+++ external/events.py
@@ -16,12 +16,20 @@ async def get_events_page(city: str, retries = 10):
             async with httpx.AsyncClient() as client:
 
                 actual_time = get_start_of_today_unix()
-                url = f'https://kudago.com/public-api/v1.4/events/?lang=&fields=id,dates,title,slug,place,description,body_text,categories,tagline,price,is_free,images,favorites_count,tags,site_url,participants&actual_since={actual_time}&location={city}'
+                url = f'https://kudago.com/public-api/v1.4/events/?lang=&fields=id,dates,title,slug,place,description,body_text,categories,tagline,price,is_free,images,favorites_count,tags,site_url,participants&actual_since={actual_time}&location={city}&expand=place'
 
                 response = await client.get(url)
                 data = response.json()
 
                 for event in data["results"]:
+
+                    if event["place"] is not None:
+
+                        place_dict = {"title": event["place"]["title"],
+                                      "address": event["place"]["address"]}
+
+                        event["place"] = place_dict
+
                     event["parsed_description"] = parse_input(event["description"])
                     event["parsed_body_text"] = parse_input(event["body_text"])
 
--- weather_api/weather_loader.py
+++ weather_api/weather_loader.py
@@ -5,9 +5,9 @@ import json
 
 load_dotenv()
 
-api_key = os.getenv('API_KEY')
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
-cities_names = {
+CITIES_NAMES = {
     "msk": "Moscow",
     "spb": "Saint Petersburg",
     "ekb": "Ekaterinburg",
@@ -15,7 +15,7 @@ cities_names = {
 
 def get_weather(city: str):
     response_list = []
-    url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={cities_names[city]}&aqi=no"'
+    url = f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={CITIES_NAMES[city]}&aqi=no"'
     response = requests.get(url)
     response_json = response.json()
     return response_json
@@ -25,12 +25,11 @@ def fill_fake_weather_db():
 
     db_path = os.path.abspath("./fake_weather_db.json")
     response_list = []
-    for city in cities_names.keys():
+    for city in CITIES_NAMES.keys():
         response_list.append(get_weather(city))
     with open(f'{db_path}', 'a', encoding='utf-8') as f:
         json.dump(response_list, f, ensure_ascii=False, indent=4)
-    print(response_list)
-    print('got weather!')
+
 
 if __name__ == "__main__":
     pass
