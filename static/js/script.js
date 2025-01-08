ec5ef1c820fce2ec8140b642a7c0d76df197d55d
Kuchev Anton <kuchevanton@bk.ru>
2025-01-08 16:10:27 +0300

    added styles & refactored html conception

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,26 @@
 async function submitForm(event) {
-            event.preventDefault(); // Предотвращаем перезагрузку страницы
+    event.preventDefault(); // Предотвращаем перезагрузку страницы
 
-            const formData = new FormData(event.target);
-            const textInputValue = document.getElementById("text-input").value;
-            formData.append("prompt", textInputValue);
-            const response = await fetch("/list", {
-                method: "POST",
-                body: formData
-            });
-            // Получаем HTML-ответ
-            const html = await response.text();
+    const formData = new FormData(event.target);
+    const textInputValue = document.getElementById("text-input").value;
+    formData.append("prompt", textInputValue);
 
-            // Вставляем HTML-ответ в элемент с id="result-container"
-            document.getElementById("result-container").innerHTML = html;
-        }
+    // Отображаем индикатор загрузки
+    document.getElementById("result-container").innerHTML = "Загрузка...";
+
+    try {
+        const response = await fetch("/list", {
+            method: "POST",
+            body: formData
+        });
+
+        // Получаем HTML-ответ
+        const html = await response.text();
+
+        // Вставляем HTML-ответ в элемент с id="result-container"
+        document.getElementById("result-container").innerHTML = html;
+    } catch (error) {
+        console.error("Ошибка при загрузке данных:", error);
+        document.getElementById("result-container").innerHTML = "Произошла ошибка. Попробуйте снова.";
+    }
+}
